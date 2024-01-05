f35dbba3d8a56e4b6dbbfb9aee7c20343e9c535a
Michael Saxon <saxon@ucsb.edu>
2024-01-05 00:49:55 -0700

    cleaning files

---
+++ vqa_scores/remove_bad_linebreaks.py
@@ -0,0 +1,30 @@
+import click
+
+import shutil
+
+# convert the multiple choice outputs into a single choice using sentencebert from a csv output in Michael's style
+@click.command()
+@click.option('--fix')
+@click.option('--endcap', default="</s>\n")
+def main(fix, endcap):
+    shutil.copyfile(fix, fix + ".bak")
+
+    with open(fix, "r") as f:
+        fix_lines = f.readlines()
+
+    # iterate through fix lines and reference lines
+    i = 1
+    while i < len(fix_lines):
+        # if the endcap isn't at the end of the line, delete the /n and merge with next line until satisfied
+        while not fix_lines[i].endswith(endcap):
+            fix_lines[i] = fix_lines[i].strip("\n") + fix_lines[i+1]
+            del fix_lines[i+1]
+            print(fix_lines[i])
+        i += 1
+    
+    with open(fix, "w") as f:
+        f.writelines(fix_lines)
+
+
+if __name__ == "__main__":
+    main()
