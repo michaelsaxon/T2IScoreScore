2fbf98a5f9f80e2c87fffb0ce81bde5646a6a0a7
Arenaa <khoshnoodi.ma@gmail.com>
2024-02-18 21:58:49 +0330

    modified dsg question generator

--- src/question_generator/dsg_question_generator.py
+++ src/question_generator/dsg_question_generator.py
@@ -4,13 +4,15 @@ import openai
 import pandas as pd
 from DSG.openai_utils import openai_completion
 from DSG.query_utils import generate_dsg
+from parse_utils import parse_dependency_output
+
 
 class DSG_QuestionGenerator:
 
     def process_prompts(self, input_file_path, csv_output_path):
         prompts = self.read_prompts_from_file(input_file_path)
 
-        csv_data = {'id': [], 'prompt': [], 'question_id': [], 'question': [], 'choices': [], 'answer': []}
+        csv_data = {'id': [], 'prompt': [], 'question_id': [], 'parent_question_id': [],  'question': [], 'choices': [], 'answer': []}
 
         for i, prompt in enumerate(prompts):
             INPUT_TEXT_PROMPT = prompt
@@ -24,6 +26,10 @@ class DSG_QuestionGenerator:
                 id2prompts,
                 generate_fn=openai_completion)
 
+            qid2dependency = parse_dependency_output(id2dependency_outputs[f'{i}']['output'])
+            for key, values in qid2dependency.items():
+              qid2dependency[key] = [value - 1 for value in values]
+
               # Extract and store the questions in a list
             questions = id2question_outputs[f'{i}']['output'].split('\n')
             questions = [q.strip().split('|')[-1].strip() for q in questions if q.strip()]  # Extract the question part
@@ -31,10 +37,17 @@ class DSG_QuestionGenerator:
             for j, question in enumerate(questions):
                 csv_data['id'].append(f'{i}')
                 csv_data['prompt'].append(prompt)
-                csv_data['question'].append(question)
+                csv_data['question'].append(question.replace(',', ''))
                 csv_data['question_id'].append(f'{j}')
+                parent_id = qid2dependency.get(j+1)
+                if parent_id:
+                    parent_id_string = ','.join(str(id) for id in parent_id)
+                    csv_data['parent_question_id'].append(parent_id_string.replace(',', '-'))
+                else:
+                    csv_data['parent_question_id'].append('')
                 csv_data['choices'].append("|".join(["yes", "no"]))
                 csv_data['answer'].append("yes")
+
 
         df = pd.DataFrame(csv_data)
         df.to_csv(csv_output_path, index=False)
